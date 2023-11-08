c1b55732b1a83f1697435816668b5d3164e1deea
jackrankin <97968661+jackrankin@users.noreply.github.com>
2023-11-08 15:23:37 -0600

    Added some deck functionality

--- frontend/cardgame_frontend/src/components/deck.js
+++ frontend/cardgame_frontend/src/components/deck.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+const values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
 
-const suits = [
-    "Diamonds",
-    "Spades",
-    "Clubs",
-    "Hearts"
-]
+function Card({card}){
+    return (
+        <div>
+            {card.suit}
+            
+        </div>
+    )
+}
 
-export default class Deck extends React.Component {
+function Deck(){
 
-    render() {
-        return (
-            <h2>
-                A DECK???
-            </h2>
-        )
-    }
+    const [deck, setDeck] = useState([]);
+
+    useEffect(() => {
+
+        const initializeDeck = () => {
+            const d = [];
+            for (var i = 0; i < 4; i++) {
+                for (var j = 0; j < 13; j++){
+                    d.push({
+                        "suit" : suits[i],
+                        "value" : values[j],
+                    })
+                }
+            }
+            setDeck(d);
+        }
+
+        initializeDeck();
+        console.log(deck);
+    }, []);
+
+    return (
+        <div>
+            {deck.length > 0 ? <Card card={deck[0]}/> : <></>}
+        </div>
+    )
+
 }
     
+export default Deck
