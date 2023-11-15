2aa46d41987076b8e8b979c1e9f43b61f4ff15a4
Connor Mahony <cmahony2@huskers.unl.edu>
2023-11-14 21:25:49 -0600

    Connection String not working

--- App/CardGame/CardGame/SQL/ConnectionAccessor.cs
+++ App/CardGame/CardGame/SQL/ConnectionAccessor.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Numerics;
 
-namespace SQL
+namespace CardGame.SQL
 {
     public class ConnectionAccessor
     {
-        private string connectionString = "Data Source =CONNORM-LAPTOP\\SQLEXPRESS;initial catalog=master;trusted_connection=true";
+        private static string _connectionString = "Data Source =CONNORM-LAPTOP\\SQLEXPRESS;initial catalog=master;trusted_connection=true";
         public ConnectionAccessor()
         {
             
@@ -14,22 +15,27 @@ namespace SQL
 
         public static void TestDatabaseConnection()
         {
-            SqlConnection connection = new SqlConnection(connectionString);
+            SqlConnection connection = new SqlConnection(_connectionString);
             try
             {
                 connection.Open();
                 if (connection.State == ConnectionState.Open)
                 {
-                    Console.WriteLine("Connection Successful!");
+                    System.Diagnostics.Debug.WriteLine("Connection Successful!\n\n\n\n");
+                    string selectQuery =
+                        "insert into Player(username, password, gamesPlayed, gamesWon, solitaireELO, warELO) values ('cjdimes12', 'jpxfr33d', 5 , 4, 1300, 2500);";
+                    using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                    {
+                        int rowsAffected = command.ExecuteNonQuery();
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Connection Unsuccsessful");
+                else {
+                    System.Diagnostics.Debug.WriteLine("Connection Unsuccsessful\n\n\n\n");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("connection failed");
+                System.Diagnostics.Debug.WriteLine("connection failed\n\n\n\n");
             }
             connection.Close();
         }
