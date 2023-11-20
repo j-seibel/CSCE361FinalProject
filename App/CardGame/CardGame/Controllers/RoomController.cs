c62f91960e59f383f7c6c6498edf83c8ee50cd0f
j-seibel <107728907+j-seibel@users.noreply.github.com>
2023-11-20 11:53:31 -0600

    Set up room creation

---
+++ App/CardGame/CardGame/Controllers/RoomController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using CardGame.Model;
+using System.Reflection;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CardGame.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Room")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Room room)
+        {
+            System.Diagnostics.Debug.WriteLine("Request Received");
+            // Access model properties here
+            System.Diagnostics.Debug.WriteLine($"Property1: {room.Id}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.RoomCode}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.Name}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.Host}");
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put([FromBody] Room room)
+        {
+            System.Diagnostics.Debug.WriteLine("Request Received");
+            // Access model properties here
+            System.Diagnostics.Debug.WriteLine($"Property1: {room.Id}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.RoomCode}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.Name}");
+            System.Diagnostics.Debug.WriteLine($"Property2: {room.Host}");
+
+
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ App/CardGame/CardGame/Model/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace CardGame.Model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomCode { get; set; }
+        public string Name { get; set; }
+        
+        public string Host { get; set; }
+    }
+}
