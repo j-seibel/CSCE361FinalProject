b86424aa891342ecf741b39fd793669d44c08a07
j-seibel <107728907+j-seibel@users.noreply.github.com>
2023-11-20 13:30:42 -0600

    Connected Room request

--- App/CardGame/CardGame/Controllers/RoomController.cs
+++ App/CardGame/CardGame/Controllers/RoomController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using CardGame.Model;
 using System.Reflection;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json;
 
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -30,14 +32,25 @@ namespace CardGame.Controllers
 
         // POST api/<ValuesController>
         [HttpPost]
+        [EnableCors]
         public void Post([FromBody] Room room)
         {
-            System.Diagnostics.Debug.WriteLine("Request Received");
-            // Access model properties here
-            System.Diagnostics.Debug.WriteLine($"Property1: {room.Id}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.RoomCode}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.Name}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.Host}");
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("Request Received");
+                // Access model properties here
+                System.Diagnostics.Debug.WriteLine($"Property1: {room.name}");
+                System.Diagnostics.Debug.WriteLine($"Property2: {room.roomID}");
+                System.Diagnostics.Debug.WriteLine($"Property2: {room.userId}");
+                // Your existing code
+            }
+            catch (Exception ex)
+            {
+                // Log or inspect the exception details
+                System.Diagnostics.Debug.WriteLine($"Fuck: {ex.Message}");
+                throw; // Rethrow the exception if necessary
+            }
+            
         }
 
         // PUT api/<ValuesController>/5
@@ -46,10 +59,10 @@ namespace CardGame.Controllers
         {
             System.Diagnostics.Debug.WriteLine("Request Received");
             // Access model properties here
-            System.Diagnostics.Debug.WriteLine($"Property1: {room.Id}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.RoomCode}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.Name}");
-            System.Diagnostics.Debug.WriteLine($"Property2: {room.Host}");
+            // System.Diagnostics.Debug.WriteLine($"Property1: {room.Id}");
+            // System.Diagnostics.Debug.WriteLine($"Property2: {room.RoomCode}");
+            // System.Diagnostics.Debug.WriteLine($"Property2: {room.Name}");
+            // System.Diagnostics.Debug.WriteLine($"Property2: {room.Host}");
 
 
         }
