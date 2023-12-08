c4a81cf8820f79404cefeedd6463e8f52d8c77bf
[Teighan Peterson} <89430237+teighannn@users.noreply.github.com>
2023-12-08 12:07:16 -0600

    (broken) War UI

--- frontend/cardgame_frontend/src/pages/roomPage/CardProp2.js
+++ frontend/cardgame_frontend/src/pages/roomPage/CardProp2.js
@@ -1,12 +1,32 @@
-import React from 'react';
+// CardProp2.js
+import React, { Component } from 'react';
 
-const CardProp2 = ({ Suit, Value, Image }) => {
-  return (
-    <div>
-      <img src={Image} alt={`Card: ${Value} of ${Suit}`} />
-      <p>{Value} of {Suit}</p>
-    </div>
-  );
-};
+class CardProp2 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      suit: props.Suit,
+      value: props.Value,
+      image: props.Image,
+      isFacing: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      image: nextProps.Image,
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ width: 125, display: 'inline-block', textAlign: 'center' }}>
+        <p>{`${this.state.value} of ${this.state.suit}`}</p>
+        <img src={this.state.image} alt={`${this.state.value} of ${this.state.suit} card`} className="card" style={{ maxWidth: '100%' }} />
+      </div>
+    );
+  }
+}
 
 export default CardProp2;
--- frontend/cardgame_frontend/src/pages/roomPage/war.js
+++ frontend/cardgame_frontend/src/pages/roomPage/war.js
@@ -1,15 +1,39 @@
+// war.js
 import React, { useState } from 'react';
 import CardProp2 from './CardProp2';
+import cards4War from '.cards4War';
 
 const War = () => {
-  const [card1, setCard1] = useState({ Suit: 'Hearts', Value: 'Ace', Image: 'path/to/card1.png' });
-  const [card2, setCard2] = useState({ Suit: 'Spades', Value: 'King', Image: 'path/to/card2.png' });
 
-  const handleNextClick = () => {
-    setCard1({ Suit: 'Diamonds', Value: '7', Image: 'path/to/card3.png' });
-    setCard2({ Suit: 'Clubs', Value: 'Queen', Image: 'path/to/card4.png' });
+  const generateImage = (value, suit) => {
+        const imageName = `${value.toLowerCase()}_of_${suit.toLowerCase()}.png`;
+        return cards4War + imageName;
   };
 
+  const [card1, setCard1] = useState({ Suit: 'Spades', Value: 'Ace', Image: generateImage('Spades', 'Ace') });
+  const [card2, setCard2] = useState({ Suit: 'Spades', Value: 'King', Image: generateImage('Spades', 'Ace') });
+
+  const handleNextClick = () => {
+    const newCard = generateRandomCard();
+    setCard1(newCard);
+    setCard2(newCard);
+  };
+
+  const generateRandomCard = () => {
+    const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+    const values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+  
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+  
+    return {
+      Suit: randomSuit,
+      Value: randomValue,
+      Image: `../cards/${randomValue.toLowerCase()}_of_${randomSuit.toLowerCase()}.png`,
+    };
+  };
+
+  
   return (
     <div>
       <CardProp2 {...card1} />
